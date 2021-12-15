2b996c28b2bd0c5e0c55ac2390a75ed1f1558b73
Glitch (rock-paper-scissors-david-valentina) <none>
2021-12-15 23:23:24 +0000

    ⚽️🎍 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -13,49 +13,60 @@ function handleLoser() {
   $(".result").css("color", "red");
 }
 
-let globalNumber = 0;
-$(".play").click(function() {
+function getRandomComputerChoice() {
   let computerNumber;
   computerNumber = Math.random() * 3;
   computerNumber = Math.ceil(computerNumber);
-  // "computer choice"
+  
+  //computer choice
   $(".computerChoice").text(`computer choice is ${computerNumber}`);
+  
+  if (computerNumber === 1) {
+    $(".computerChoice").text("Rock");
+  }
+  else if (computerNumber === 2) {
+    $(".computerChoice").text("Scissors");
+  }
+  else if (computerNumber === 3) {
+    $(".computerChoice").text("Paper");
+  }
+  
+  return computerNumber;
+}
+
+
+
+let globalNumber = 0;
+$(".play").click(function() {
 
   let userInput = $(".input").val();
   userInput = userInput.toLowerCase();
 
   $(".userChoice").text(userInput);
+  
+  let computerAnswer= getRandomComputerChoice();
 
-  if (computerNumber === 1) {
-    $(".computerChoice").text("Rock");
-  }
-  if (computerNumber === 2) {
-    $(".computerChoice").text("Scissors");
-  }
-  if (computerNumber === 3) {
-    $(".computerChoice").text("Paper");
-  }
 
-  if (computerNumber === 1 && userInput == "rock") {
-    handleTie();
-  } else if (computerNumber === 1 && userInput === "paper") {
-    handleWinner();
-  } else if (computerNumber === 1 && userInput === "scissors") {
-    handleLoser();
-  } else if (computerNumber === 2 && userInput === "scissors") {
-    handleTie();
-  } else if (computerNumber === 2 && userInput === "paper") {
-    handleLoser();
-  } else if (computerNumber === 2 && userInput === "rock") {
-    handleWinner();
-  } else if (computerNumber === 3 && userInput === "paper") {
-    handleTie();
-  } else if (computerNumber === 3 && userInput === "rock") {
-    handleLoser();
-  } else if (computerNumber === 3 && userInput === "scissors") {
-    handleWinner();
+  if (computerAnswer === 1 && userInput == "rock") {
+      handleTie();
+  } else if (computerAnswer === 1 && userInput === "paper") {
+      handleWinner();
+  } else if (computerAnswer === 1 && userInput === "scissors") {
+      handleLoser();
+  } else if (computerAnswer === 2 && userInput === "scissors") {
+      handleTie();
+  } else if (computerAnswer === 2 && userInput === "paper") {
+      handleLoser();
+  } else if (computerAnswer === 2 && userInput === "rock") {
+      handleWinner();
+  } else if (computerAnswer === 3 && userInput === "paper") {
+      handleTie();
+  } else if (computerAnswer === 3 && userInput === "rock") {
+      handleLoser();
+  } else if (computerAnswer === 3 && userInput === "scissors") {
+      handleWinner();
   } else {
-    $(".result").text(
+      $(".result").text(
       "oops... looks like there's a mistake in your input. Try checking the image to make sure you spelled your input correctly. And make sure to use capital letters!"
     );
   }
